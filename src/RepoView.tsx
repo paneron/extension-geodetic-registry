67b1499061173dce00ca9d76d468a6a1c2196b5b
Anton Strogonoff <anton@strogonoff.name>
2020-09-24 16:13:31 +0900

    chore(RepoView): Check if host imports work

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from '@blueprintjs/core';
 import { RepositoryViewProps } from './types';
 
 
 export const RepoView: React.FC<RepositoryViewProps> = function ({ title }) {
-  return <p>Welcome to {title}</p>;
-}
+  return <Button>Welcome to {title}</Button>;
+};
