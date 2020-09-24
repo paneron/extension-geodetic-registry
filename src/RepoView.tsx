2c1ef8009b9a3ee42573ef5bc7f0abe05e502869
Anton Strogonoff <anton@strogonoff.name>
2020-09-24 20:17:03 +0900

    chore(RepoView): placeholder message

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Button } from '@blueprintjs/core';
+import { NonIdealState } from '@blueprintjs/core';
 import { RepositoryViewProps } from './types';
 
 
-export const RepoView: React.FC<RepositoryViewProps> = function ({ title }) {
-  return <Button>Welcome to {title}</Button>;
+export const RepoView: React.FC<RepositoryViewProps> = function () {
+  return <NonIdealState
+    icon="time"
+    title="Check back in a bit!"
+    description="Geodetic Registry repository view is coming soon" />;
 };
