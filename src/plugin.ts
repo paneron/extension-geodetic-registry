01f33e4f6895c7be1d31a5bca0dffe56967a2fd8
Anton Strogonoff <anton@strogonoff.name>
2021-03-21 08:18:10 +0900

    chore(deps): bump extension & registry kit versions

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,9 @@
 import { makeExtension } from '@riboseinc/paneron-extension-kit';
 
 export default makeExtension({
-  repoView: () => import('./RepoView'),
+  mainView: () => import('./RepoView'),
   name: "Geodetic registry",
+  requiredHostAppVersion: "^1.0.0-alpha19",
+  datasetMigrations: {},
+  datasetInitializer: () => import('./migrations/initial'),
 });
