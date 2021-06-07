d39906caca3bfa6850fdc5eb442110d4f92ebc06
Anton Strogonoff <anton@strogonoff.name>
2021-06-07 17:13:54 +0300

    chore(repo view): specify default criteria

--- src/RepoView.tsx
+++ src/RepoView.tsx
@@ -6,9 +6,21 @@ import { jsx } from '@emotion/core';
 Object.assign(console, log);
 
 import { RegistryView } from '@riboseinc/paneron-registry-kit/views';
+import CRITERIA_CONFIGURATION from '@riboseinc/paneron-registry-kit/views/FilterCriteria/CRITERIA_CONFIGURATION';
 import { itemClassConfiguration } from './registryConfig';
 
 
+const defaultClassID = Object.keys(itemClassConfiguration)[0];
+const defaultCriteria = CRITERIA_CONFIGURATION['item-class'].toQuery(
+  { classID: defaultClassID },
+  { itemClasses: itemClassConfiguration },
+);
+
+
 export default function () {
-  return <RegistryView itemClassConfiguration={itemClassConfiguration} />
+  return <RegistryView
+    itemClassConfiguration={itemClassConfiguration}
+    keyExpression="obj.data.identifier || obj.id"
+    defaultSearchCriteria={{ require: 'all', criteria: [{ key: 'item-class', query: defaultCriteria }] }}
+  />
 };
