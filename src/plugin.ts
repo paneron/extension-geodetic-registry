36f1149c9e8df73bc19b4cfea32552c54280db8d
Anton Strogonoff <anton@strogonoff.name>
2021-06-07 11:51:46 +0300

    chore(package.json): specify Paneron beta as required version

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,7 @@ import { makeExtension } from '@riboseinc/paneron-extension-kit';
 export default makeExtension({
   mainView: () => import('./RepoView'),
   name: "Geodetic registry",
-  requiredHostAppVersion: "^1.0.0-alpha19",
+  requiredHostAppVersion: "^1.0.0-beta1",
   datasetMigrations: {},
   datasetInitializer: () => import('./migrations/initial'),
 });
