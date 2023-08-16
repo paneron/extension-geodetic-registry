b6507d559cc846c914ccdc5b7f72deb125110d69
Anton <anton@strogonoff.name>
2023-08-16 18:30:43 +0800

    fix(crs): proper support for CRS features

--- src/classes/crs.tsx
+++ src/classes/crs.tsx
@@ -2,35 +2,42 @@
 
 import update from 'immutability-helper';
 import { jsx } from '@emotion/react';
-import { H5 } from '@blueprintjs/core';
-import { type ItemClassConfiguration, ItemDetailView, ItemEditView, ItemListView } from '@riboseinc/paneron-registry-kit/types';
+import type {
+  ItemClassConfiguration,
+  ItemDetailView,
+  ItemEditView,
+  ItemListView,
+} from '@riboseinc/paneron-registry-kit/types';
 import { PropertyDetailView } from '@riboseinc/paneron-registry-kit/views/util';
 import GenericRelatedItemView from '@riboseinc/paneron-registry-kit/views/GenericRelatedItemView';
 
 import {
   type CommonGRItemData,
   DEFAULTS as SHARED_DEFAULTS,
+  COMMON_PROPERTIES,
   EditView as CommonEditView,
   ListItemView as CommonListItemView,
   DetailView as CommonDetailView,
   type Extent,
   DEFAULT_EXTENT,
   ExtentEdit,
-  COMMON_PROPERTIES,
+  RelatedField,
 } from './common';
 
+
+// Shared
+// ======
 
 export interface CRSData extends CommonGRItemData {
   scope: string
   extent: Extent
-  coordinateSystem?: { classID: string, itemID: string }
 }
 
 export const CRS_DEFAULTS: CRSData = {
   ...SHARED_DEFAULTS,
   extent: DEFAULT_EXTENT,
   scope: '',
-}
+};
 
 const CRSDetailView: ItemDetailView<CRSData> = function (props) {
   const data = props.itemData;
@@ -43,20 +50,9 @@ const CRSDetailView: ItemDetailView<CRSData> = function (props) {
 
       {props.children}
 
-
-      <H5>Coordinate system</H5>
-      {data.coordinateSystem
-        ? <GenericRelatedItemView
-            itemRef={data.coordinateSystem}
-            getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-            useRegisterItemData={props.useRegisterItemData}
-          />
-        : '—'}
-
     </CommonDetailView>
   );
 };
-
 
 const CRSEditView: ItemEditView<CRSData> = function (props) {
   return (
@@ -81,130 +77,84 @@ const CRSEditView: ItemEditView<CRSData> = function (props) {
 
       {props.children}
 
-      <PropertyDetailView title="Coordinate system">
-        <GenericRelatedItemView
-          itemRef={props.itemData.coordinateSystem}
-          availableClassIDs={['coordinate-sys--cartesian', 'coordinate-sys--ellipsoidal', 'coordinate-sys--vertical']}
-          onClear={props.onChange
-            ? () => props.onChange!(update(props.itemData, { $unset: ['coordinateSystem'] }))
-            : undefined}
-          onChange={props.onChange
-            ? (itemRef) => {
-                props.onChange!(update(props.itemData, { coordinateSystem: { $set: itemRef } }))
-              }
-            : undefined}
-          itemSorter={COMMON_PROPERTIES.itemSorter}
-          getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-          useRegisterItemData={props.useRegisterItemData}
-        />
-      </PropertyDetailView>
-
     </CommonEditView>
   );
 };
 
 
+// Shared for non-compound
+// =======================
+
+export interface NonCompoundCRSData extends CRSData {
+  coordinateSystem?: {
+    /** Ellipsoidal, spherical, vertical, cartesian */
+    classID: string
+    itemID: string
+  }
+  baseCRS?: {
+    /** TODO: Which types are possible? */
+    classID: string
+    itemID: string
+  }
+  operation?: {
+    /** Transformation or Conversion */
+    classID: string
+    itemID: string
+  }
+}
+
+export const NON_COMPOUND_CRS_DEFAULTS: CRSData = {
+  ...SHARED_DEFAULTS,
+  extent: DEFAULT_EXTENT,
+  scope: '',
+}
+
+const NonCompoundCRSEditView: ItemEditView<NonCompoundCRSData> = function (props) {
+  return (
+    <CRSEditView
+        useRegisterItemData={props.useRegisterItemData}
+        getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
+        itemData={props.itemData}
+        itemRef={props.itemRef}
+        onChange={props.onChange ? (newData: CommonGRItemData) => {
+          if (!props.onChange) { return; }
+          props.onChange({ ...props.itemData, ...newData });
+        } : undefined}>
+
+      <PropertyDetailView title="Coordinate system">
+        <RelatedField
+          fieldName="coordinateSystem"
+          allowedClasses={[
+            'coordinate-sys--cartesian',
+            'coordinate-sys--vertical',
+            'coordinate-sys--ellipsoidal',
+            'coordinate-sys--spherical',
+          ]}
+          {...props}
+        />
+      </PropertyDetailView>
+
+      {props.children}
+
+    </CRSEditView>
+  );
+};
+
+
+// Concrete classes
+// ================
+
 export interface CompoundCRSData extends CRSData {
   verticalCRS: { classID: string, itemID: string }
   horizontalCRS: { classID: string, itemID: string }
 }
+
 export const COMPOUND_DEFAULTS = {
   horizontal: undefined,
-  vertical: undefined
-}
-
-export interface ProjectedCRSData extends CRSData {
-}
-export const PROJECTED_DEFAULTS = {
-}
-
-
-export interface EngineeringCRSData extends CRSData {
-  datum: string // engineering
-}
-export const ENGINEERING_DEFAULTS = {
-  datum: ''
-}
-
-
-export interface VerticalCRSData extends CRSData {
-  datum: string // vertical
-}
-export const VERTICAL_DEFAULTS = {
-  datum: ''
-}
-
-
-
-export interface GeodeticCRSData extends CRSData {
-  /** Geodetic datum */
-  datum: string
-}
-export const GEODETIC_DEFAULTS = {
-  datum: ''
-}
-
-export const engineeringCRS: ItemClassConfiguration<EngineeringCRSData> = {
-  ...COMMON_PROPERTIES,
-  meta: {
-    title: "Engineering CRS",
-    description: "Engineering Coordinate Reference System",
-    id: 'crs--engineering',
-    alternativeNames: [],
-  },
-  defaults: {
-    ...CRS_DEFAULTS,
-    ...ENGINEERING_DEFAULTS,
-  },
-  views: {
-    listItemView: CommonListItemView as ItemListView<EngineeringCRSData>,
-    detailView: (props) => {
-      const data = props.itemData;
-
-      return (
-        <CRSDetailView {...props}>
-
-          {data.datum
-            ? <PropertyDetailView title="Datum">
-                <GenericRelatedItemView
-                  itemRef={{ classID: 'datums--engineering', itemID: data.datum }}
-                  getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-                  useRegisterItemData={props.useRegisterItemData}
-                />
-              </PropertyDetailView>
-            : '—'}
-
-        </CRSDetailView>
-      )
-    },
-    editView: (props) => {
-      const EditView = CRSEditView as ItemEditView<EngineeringCRSData>;
-      return (
-        <EditView {...props}>
-          <PropertyDetailView title="Datum (engineering)">
-            <GenericRelatedItemView
-              itemRef={{ itemID: props.itemData.datum, classID: 'datums--engineering' }}
-              availableClassIDs={['datums--engineering']}
-              onClear={props.onChange
-                ? () => props.onChange!(update(props.itemData, { $unset: ['datum'] }))
-                : undefined}
-              onChange={props.onChange
-                ? (itemRef) => {
-                    props.onChange!(update(props.itemData, { datum: { $set: itemRef.itemID } }))
-                  }
-                : undefined}
-              itemSorter={COMMON_PROPERTIES.itemSorter}
-              getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-              useRegisterItemData={props.useRegisterItemData}
-            />
-          </PropertyDetailView>
-        </EditView>
-      );
-    },
-  },
-  validatePayload: async () => true,
-  sanitizePayload: async (t) => t,
+  vertical: undefined,
 };
+
+
 
 export const compoundCRS: ItemClassConfiguration<CompoundCRSData> = {
   ...COMMON_PROPERTIES,
@@ -252,37 +202,24 @@ export const compoundCRS: ItemClassConfiguration<CompoundCRSData> = {
       return (
         <EditView {...props}>
           <PropertyDetailView title="Horizontal CRS">
-            <GenericRelatedItemView
-              itemRef={props.itemData.horizontalCRS}
-              availableClassIDs={['crs--projected', 'crs--engineering', 'crs--geodetic']}
-              onClear={props.onChange
-                ? () => props.onChange!(update(props.itemData, { $unset: ['horizontalCRS'] }))
-                : undefined}
-              onChange={props.onChange
-                ? (itemRef) => {
-                    props.onChange!(update(props.itemData, { horizontalCRS: { $set: itemRef } }))
-                  }
-                : undefined}
-              itemSorter={COMMON_PROPERTIES.itemSorter}
-              getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-              useRegisterItemData={props.useRegisterItemData}
+            <RelatedField
+              fieldName="horizontalCRS"
+              allowedClasses={[
+                'crs--projected',
+                'crs--engineering',
+                'crs--geodetic',
+              ]}
+              {...props}
             />
           </PropertyDetailView>
           <PropertyDetailView title="Vertical CRS">
-            <GenericRelatedItemView
-              itemRef={props.itemData.verticalCRS}
-              availableClassIDs={['crs--vertical', 'crs--engineering']}
-              onClear={props.onChange
-                ? () => props.onChange!(update(props.itemData, { $unset: ['verticalCRS'] }))
-                : undefined}
-              onChange={props.onChange
-                ? (itemRef) => {
-                    props.onChange!(update(props.itemData, { verticalCRS: { $set: itemRef } }))
-                  }
-                : undefined}
-              itemSorter={COMMON_PROPERTIES.itemSorter}
-              getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-              useRegisterItemData={props.useRegisterItemData}
+            <RelatedField
+              fieldName="verticalCRS"
+              allowedClasses={[
+                'crs--vertical',
+                'crs--engineering',
+              ]}
+              {...props}
             />
           </PropertyDetailView>
         </EditView>
@@ -291,6 +228,13 @@ export const compoundCRS: ItemClassConfiguration<CompoundCRSData> = {
   },
   validatePayload: async () => true,
   sanitizePayload: async (t) => t,
+};
+
+
+export interface ProjectedCRSData extends NonCompoundCRSData {
+}
+
+export const PROJECTED_DEFAULTS = {
 };
 
 export const projectedCRS: ItemClassConfiguration<ProjectedCRSData> = {
@@ -307,22 +251,62 @@ export const projectedCRS: ItemClassConfiguration<ProjectedCRSData> = {
   },
   views: {
     listItemView: CommonListItemView as ItemListView<ProjectedCRSData>,
-    detailView: (props) => {
-      return (
-        <CRSDetailView {...props}>
-        </CRSDetailView>
-      )
-    },
+    editView: NonCompoundCRSEditView,
+  },
+  validatePayload: async () => true,
+  sanitizePayload: async (t) => t,
+};
+
+
+export interface VerticalCRSData extends NonCompoundCRSData {
+  datum: string // vertical
+}
+
+export const VERTICAL_DEFAULTS = {
+  datum: '',
+};
+
+export const verticalCRS: ItemClassConfiguration<VerticalCRSData> = {
+  ...COMMON_PROPERTIES,
+  meta: {
+    title: "Vertical CRS",
+    description: "Vertical Coordinate Reference System",
+    id: 'crs--vertical',
+    alternativeNames: [],
+  },
+  defaults: {
+    ...CRS_DEFAULTS,
+    ...VERTICAL_DEFAULTS,
+  },
+  views: {
+    listItemView: CommonListItemView as ItemListView<VerticalCRSData>,
     editView: (props) => {
-      const EditView = CRSEditView as ItemEditView<ProjectedCRSData>;
+      const EditView = NonCompoundCRSEditView as ItemEditView<VerticalCRSData>;
       return (
         <EditView {...props}>
+          <PropertyDetailView title="Datum (vertical)">
+            <RelatedField
+              fieldName="datum"
+              relatedClassID="datums--vertical"
+              {...props}
+            />
+          </PropertyDetailView>
         </EditView>
       );
     },
   },
   validatePayload: async () => true,
   sanitizePayload: async (t) => t,
+};
+
+
+export interface GeodeticCRSData extends NonCompoundCRSData {
+  /** Geodetic datum */
+  datum: string
+}
+
+export const GEODETIC_DEFAULTS = {
+  datum: '',
 };
 
 export const geodeticCRS: ItemClassConfiguration<GeodeticCRSData> = {
@@ -339,44 +323,15 @@ export const geodeticCRS: ItemClassConfiguration<GeodeticCRSData> = {
   },
   views: {
     listItemView: CommonListItemView as ItemListView<GeodeticCRSData>,
-    detailView: (props) => {
-      const data = props.itemData;
-
-      return (
-        <CRSDetailView {...props}>
-
-          {data.datum
-            ? <PropertyDetailView title="Datum">
-                <GenericRelatedItemView
-                  itemRef={{ classID: 'datums--geodetic', itemID: data.datum }}
-                  getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-                  useRegisterItemData={props.useRegisterItemData}
-                />
-              </PropertyDetailView>
-            : '—'}
-
-        </CRSDetailView>
-      )
-    },
     editView: (props) => {
-      const EditView = CRSEditView as ItemEditView<GeodeticCRSData>;
+      const EditView = NonCompoundCRSEditView as ItemEditView<GeodeticCRSData>;
       return (
         <EditView {...props}>
           <PropertyDetailView title="Datum (geodetic)">
-            <GenericRelatedItemView
-              itemRef={{ itemID: props.itemData.datum, classID: 'datums--geodetic' }}
-              availableClassIDs={['datums--geodetic']}
-              onClear={props.onChange
-                ? () => props.onChange!(update(props.itemData, { $unset: ['datum'] }))
-                : undefined}
-              onChange={props.onChange
-                ? (itemRef) => {
-                    props.onChange!(update(props.itemData, { datum: { $set: itemRef.itemID } }))
-                  }
-                : undefined}
-              itemSorter={COMMON_PROPERTIES.itemSorter}
-              getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-              useRegisterItemData={props.useRegisterItemData}
+            <RelatedField
+              fieldName="datum"
+              relatedClassID="datums--geodetic"
+              {...props}
             />
           </PropertyDetailView>
         </EditView>
@@ -388,58 +343,37 @@ export const geodeticCRS: ItemClassConfiguration<GeodeticCRSData> = {
 };
 
 
-export const verticalCRS: ItemClassConfiguration<VerticalCRSData> = {
+export interface EngineeringCRSData extends NonCompoundCRSData {
+  datum: string // engineering
+}
+
+export const ENGINEERING_DEFAULTS = {
+  datum: '',
+};
+
+export const engineeringCRS: ItemClassConfiguration<EngineeringCRSData> = {
   ...COMMON_PROPERTIES,
   meta: {
-    title: "Vertical CRS",
-    description: "Vertical Coordinate Reference System",
-    id: 'crs--vertical',
+    title: "Engineering CRS",
+    description: "Engineering Coordinate Reference System",
+    id: 'crs--engineering',
     alternativeNames: [],
   },
   defaults: {
     ...CRS_DEFAULTS,
-    ...VERTICAL_DEFAULTS,
+    ...ENGINEERING_DEFAULTS,
   },
   views: {
-    listItemView: CommonListItemView as ItemListView<VerticalCRSData>,
-    detailView: (props) => {
-      const data = props.itemData;
-
-      return (
-        <CRSDetailView {...props}>
-
-          {data.datum
-            ? <PropertyDetailView title="Datum">
-                <GenericRelatedItemView
-                  itemRef={{ classID: 'datums--vertical', itemID: data.datum }}
-                  getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-                  useRegisterItemData={props.useRegisterItemData}
-                />
-              </PropertyDetailView>
-            : '—'}
-
-        </CRSDetailView>
-      )
-    },
+    listItemView: CommonListItemView as ItemListView<EngineeringCRSData>,
     editView: (props) => {
-      const EditView = CRSEditView as ItemEditView<VerticalCRSData>;
+      const EditView = NonCompoundCRSEditView as ItemEditView<EngineeringCRSData>;
       return (
         <EditView {...props}>
-          <PropertyDetailView title="Datum (vertical)">
-            <GenericRelatedItemView
-              itemRef={{ itemID: props.itemData.datum, classID: 'datums--vertical' }}
-              availableClassIDs={['datums--vertical']}
-              onClear={props.onChange
-                ? () => props.onChange!(update(props.itemData, { $unset: ['datum'] }))
-                : undefined}
-              onChange={props.onChange
-                ? (itemRef) => {
-                    props.onChange!(update(props.itemData, { datum: { $set: itemRef.itemID } }))
-                  }
-                : undefined}
-              itemSorter={COMMON_PROPERTIES.itemSorter}
-              getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-              useRegisterItemData={props.useRegisterItemData}
+          <PropertyDetailView title="Datum (engineering)">
+            <RelatedField
+              fieldName="datum"
+              relatedClassID="datums--engineering"
+              {...props}
             />
           </PropertyDetailView>
         </EditView>
