83076777c415a2ed2e105c46d94af354a81442b2
Anton <anton@strogonoff.name>
2023-11-03 19:27:40 +0800

    refactor(coordinate-op-method): ditch detailView

--- src/classes/coordinate-op-method.tsx
+++ src/classes/coordinate-op-method.tsx
@@ -1,16 +1,14 @@
 /** @jsx jsx */
 
-import { jsx, css } from '@emotion/react';
+import { jsx } from '@emotion/react';
 import { type ItemClassConfiguration, ItemListView } from '@riboseinc/paneron-registry-kit/types';
 import { PropertyDetailView } from '@riboseinc/paneron-registry-kit/views/util';
-import GenericRelatedItemView from '@riboseinc/paneron-registry-kit/views/GenericRelatedItemView';
 
 import {
   CommonGRItemData,
   DEFAULTS as SHARED_DEFAULTS,
   EditView as CommonEditView,
   ListItemView as CommonListItemView,
-  DetailView as CommonDetailView,
   COMMON_PROPERTIES,
 } from './common';
 
@@ -39,28 +37,6 @@ export const coordinateOpMethod: ItemClassConfiguration<CoordinateOpMethod> = {
   },
   views: {
     listItemView: CommonListItemView as ItemListView<CoordinateOpMethod>,
-    detailView: (props) => {
-      const data = props.itemData;
-
-      return (
-        <CommonDetailView {...props}>
-
-          {(data.parameters || []).length > 0
-            ? <PropertyDetailView title="Parameters">
-                {data.parameters.map(paramID =>
-                  <GenericRelatedItemView
-                    css={css`margin-bottom: 1em;`}
-                    getRelatedItemClassConfiguration={props.getRelatedItemClassConfiguration}
-                    itemRef={{ classID: 'coordinate-op-parameter', itemID: paramID }}
-                    useRegisterItemData={props.useRegisterItemData}
-                  />
-                )}
-              </PropertyDetailView>
-            : null}
-
-        </CommonDetailView>
-      )
-    },
     editView: (props) => (
       <CommonEditView
         useRegisterItemData={props.useRegisterItemData}
