3a5a179b00ba7d516ca59a7f4edafe8ce374a062
Anton <anton@strogonoff.name>
2023-11-13 16:10:47 +0800

    feat(concatenated-operation): validate CRS chain
    as part of #27

--- src/classes/concatenated-operation.tsx
+++ src/classes/concatenated-operation.tsx
@@ -3,11 +3,13 @@
 
 import update from 'immutability-helper';
 
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
 import { jsx } from '@emotion/react';
 import { ControlGroup, InputGroup } from '@blueprintjs/core';
 import { PropertyDetailView } from '@riboseinc/paneron-registry-kit/views/util';
-import type { InternalItemReference, ItemClassConfiguration, ItemListView } from '@riboseinc/paneron-registry-kit/types';
+import { itemRefToItemPath } from '@riboseinc/paneron-registry-kit/views/itemPathUtils';
+import { BrowserCtx } from '@riboseinc/paneron-registry-kit/views/BrowserCtx';
+import type { RegisterItem, InternalItemReference, ItemClassConfiguration, ItemListView } from '@riboseinc/paneron-registry-kit/types';
 
 import {
   type CommonGRItemData,
@@ -21,6 +23,9 @@ import {
   AccuracyEdit,
   ACCURACY_STUB,
 } from './common';
+
+import type { TransformationData } from './transformation';
+import type { ConversionData } from './conversion';
 
 
 export interface ConcatenatedOperationData extends CommonGRItemData {
@@ -53,6 +58,72 @@ export const concatenatedOperation: ItemClassConfiguration<ConcatenatedOperation
     listItemView: CommonListItemView as ItemListView<ConcatenatedOperationData>,
 
     editView: function ConcatenatedOperationEditView ({ itemData, onChange, ...props }) {
+      const { useRegisterItemData } = useContext(BrowserCtx);
+      const operationItemPaths = itemData.operations.map(ref => itemRefToItemPath(ref));
+
+      /** Item data for every linked single operation. */
+      const operationItemData = useRegisterItemData({
+        itemPaths: operationItemPaths,
+      }).value as Record<string, RegisterItem<TransformationData | ConversionData> | null>;
+
+      /**
+       * [source, target] CRS item paths for every linked single operation.
+       * Operations with either CRS missing are omitted.
+       */
+      const crsRefs: Record<string, [string, string]> = useMemo(() =>
+        Object.values(operationItemData).
+          filter(op => op !== null && op !== undefined).
+          map(op => {
+            let crsPaths: [string, string] = ['', ''];
+            if (op!.data) {
+              const opData = op!.data as any;
+              crsPaths = [
+                opData.sourceCRS ? itemRefToItemPath(opData.sourceCRS) : '',
+                opData.targetCRS ? itemRefToItemPath(opData.targetCRS) : '',
+              ];
+            }
+            return [op!.id, crsPaths] as [string, [string, string]];
+          }).
+          filter(([_, paths]) => (paths.find(p => p === '') === undefined)).
+          map(([opID, paths]) =>
+            ({ [opID]: [paths[0], paths[1]] as [string, string] })
+          ).
+          reduce((prev, curr) => ({ ...prev, ...curr }), {}),
+        [operationItemData]);
+
+      /** CRS data for every linked CRS, through linked operations. */
+      const crsItemData = useRegisterItemData({
+        itemPaths: [...Object.values(crsRefs)].flat(),
+      });
+
+      /**
+       * Maps each operation item UUID to custom validation error
+       * (empty string if no error).
+       */
+      const operationValidationErrors: Record<string, string> = useMemo(() => {
+        const ops = itemData.operations.map(ref => ref.itemID);
+        const errors: Record<string, string> = {};
+        let previousTarget: string | null = null;
+        for (const opUUID of ops) {
+          if (crsRefs[opUUID]) {
+            if (!previousTarget) {
+              previousTarget = crsRefs[opUUID][1];
+              errors[opUUID] = "";
+            } else {
+              if (crsRefs[opUUID][0] !== previousTarget) {
+                errors[opUUID] = "Source CRS of this operation does not match target CRS of previous operation";
+              } else {
+                errors[opUUID] = "";
+              }
+            }
+          } else {
+            console.warn("Source/target CRS for operation with UUID were not retrieved:", opUUID);
+            errors[opUUID] = "Unable to fetch source/target CRS for this operation.";
+          }
+        }
+        return errors;
+      }, [crsItemData]);
+
       return (
         <>
           <CommonEditView
@@ -97,6 +168,7 @@ export const concatenatedOperation: ItemClassConfiguration<ConcatenatedOperation
                 <ControlGroup>
                   <RelatedItem
                     itemRef={opRef}
+                    validity={operationValidationErrors[opRef.itemID]}
                     mode="generic"
                     classIDs={['coordinate-ops--transformation', 'coordinate-ops--conversion']}
                     onSet={handleChange
