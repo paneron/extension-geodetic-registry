22d760b3dca52b785d38501a6b369140c925c4a4
Anton <anton@strogonoff.name>
2023-11-28 14:49:40 +0700

    fix: switch to makeRegistryExtension
    TODO: Investigate why previous way of makeExtension didn’t work

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,20 +1,47 @@
-import { makeExtension } from '@riboseinc/paneron-extension-kit';
-//import exportPublicSite from '@riboseinc/paneron-registry-kit/site-builder';
-import datasetInitializer from '@riboseinc/paneron-registry-kit/migrations/initial';
-import mainView from './RepoView';
+// import { makeExtension } from '@riboseinc/paneron-extension-kit';
+// //import exportPublicSite from '@riboseinc/paneron-registry-kit/site-builder';
+// import datasetInitializer from '@riboseinc/paneron-registry-kit/migrations/initial';
+// import mainView from './RepoView';
+// 
+// 
+// export default makeExtension({
+//   mainView,
+//   name: "Geodetic registry",
+//   requiredHostAppVersion: "^2.0.0",
+//   datasetMigrations: {},
+//   datasetInitializer,
+//   exportFormats: {
+//     //['public-site']: {
+//     //  name: "Public website",
+//     //  description: "The current version of the register (excluding proposals), rendered in HTML and ready for web serving.",
+//     //  exporter: exportPublicSite,
+//     //},
+//   },
+// });
 
 
-export default makeExtension({
-  mainView,
-  name: "Geodetic registry",
-  requiredHostAppVersion: "^2.0.0",
-  datasetMigrations: {},
-  datasetInitializer,
-  exportFormats: {
-    //['public-site']: {
-    //  name: "Public website",
-    //  description: "The current version of the register (excluding proposals), rendered in HTML and ready for web serving.",
-    //  exporter: exportPublicSite,
-    //},
+import { makeRegistryExtension, CRITERIA_CONFIGURATION } from '@riboseinc/paneron-registry-kit';
+
+import { itemClassConfiguration } from './registryConfig';
+
+
+const defaultClassID = Object.keys(itemClassConfiguration)[0];
+const defaultCriteria = CRITERIA_CONFIGURATION['item-class'].toQuery(
+  { classID: defaultClassID },
+  { itemClasses: itemClassConfiguration },
+);
+
+const defaultSearchCriteria = {
+  require: 'all',
+  criteria: [{ key: 'item-class', query: defaultCriteria }],
+} as const;
+
+export default makeRegistryExtension({
+  name: "ISO Geodetic Registry",
+  itemClassConfiguration,
+  keyExpression: "obj.data.identifier || obj.id",
+  defaultSearchCriteria: defaultSearchCriteria as any,
+  getQuickSearchPredicate: function getQuickSearchPredicate(quickSearchString) {
+    return `[obj.data?.name ?? '', obj.data?.identifier?.toString()].join('').toLowerCase().indexOf("${quickSearchString.toLowerCase()}") >= 0`;
   },
 });
