35d7ee51d73a0a6a9e84a09dcb5f6a68cbbf893f
Anton <anton@strogonoff.name>
2024-01-22 18:17:23 +0800

    fix: correct logic for choosing next available ID
    Take into account other items from the same proposal.

--- src/plugin.ts
+++ src/plugin.ts
@@ -46,6 +46,9 @@ export default makeRegistryExtension({
   keyExpression: "obj.data.identifier || obj.id",
   defaultSearchCriteria: defaultSearchCriteria as any,
   alterApprovedCR: async function (crID, proposals, origItemData, newItemData, { getMapReducedData }) {
+    /** Next unoccupied identifier per class ID. */
+    const nextAvailableIDPerClass: Record<string, number> = {};
+
     for (const [itemPath, proposal] of Object.entries(proposals)) {
       const itemDataPath = itemPathInCR(itemPath, crID);
       const payload: Payload | null = newItemData[itemDataPath]?.data ?? null;
@@ -54,27 +57,35 @@ export default makeRegistryExtension({
         if (payload.identifier >= 0) {
           throw new Error("Additions must have negative identifiers at approval time");
         }
-        try {
-          const { subregisterID, classID } = itemPathToItemRef(false, itemDataPath);
-          const itemPathPrefix = incompleteItemRefToItemPathPrefix({ subregisterID, classID });
-          const newIDResult = await getMapReducedData({
-            chains: {
-              maxID: {
-                mapFunc: `
-                  if (key.startsWith("${itemPathPrefix}")) { emit(value?.data?.identifier ?? 0); }
-                `,
-                reduceFunc: `
-                  return (value > accumulator) ? (value + 1) : accumulator;
-                `,
+        const { subregisterID, classID } = itemPathToItemRef(false, itemDataPath);
+        if (!nextAvailableIDPerClass[classID]) {
+          try {
+            const itemPathPrefix = incompleteItemRefToItemPathPrefix({ subregisterID, classID });
+            const newIDResult = await getMapReducedData({
+              chains: {
+                maxID: {
+                  mapFunc: `
+                    if (key.startsWith("${itemPathPrefix}")) { emit(value?.data?.identifier ?? 1); }
+                  `,
+                  reduceFunc: `
+                    return (value > accumulator) ? value : accumulator;
+                  `,
+                },
               },
-            },
-          });
-          const maybeNum = parseInt(newIDResult.maxID, 10);
-          // Overwrite identifier with next available one
-          newItemData[itemDataPath]!.data.identifier = maybeNum;
-        } catch (e) {
-          throw new Error(`Failed to alter item identifier (${String(e)})`);
+            });
+            const maybeNum = parseInt(newIDResult.maxID, 10);
+            if (!Number.isInteger(maybeNum)) {
+              throw new Error("Obtained identifier is not an integer");
+            }
+            nextAvailableIDPerClass[classID] = maybeNum + 1;
+          } catch (e) {
+            throw new Error(`Failed to get current maximum identifier for ${classID} (${String(e)})`);
+          }
         }
+        // Overwrite identifier with next available for this class
+        newItemData[itemDataPath]!.data.identifier = nextAvailableIDPerClass[classID];
+        // Bump next available identifier
+        nextAvailableIDPerClass[classID] += 1;
       } else if (payload && payload.identifier < 0) {
         throw new Error("Items with negative identifier cannot be amended or clarified");
       } else if (preexistingData && payload && preexistingData.identifier !== payload.identifier) {
